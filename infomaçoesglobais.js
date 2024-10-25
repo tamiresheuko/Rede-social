80b5b231b5a49cccaa1bdcab10fe3bf7b7b9db5b
tamiresheuko <105224111+tamiresheuko@users.noreply.github.com>
2024-10-25 10:48:25 -0300

    salva

---
+++ infomaçoesglobais.js
@@ -0,0 +1,7 @@
+const url='https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+
+async function visualizarinformaçoesglobais() {
+const res = await fetch(url);
+const dados= res.json()
+console.log(dados)
+}
